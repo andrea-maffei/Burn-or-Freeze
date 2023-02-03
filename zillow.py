4730378cd32cb30322873e18deda15ddd79f1203
Andrea Maffei <andrea.maff.usa@gmail.com>
2023-02-03 18:57:47 -0500

    this is test to see if the email will work and there was a change in the code

--- zillow.py
+++ zillow.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+import pandas as pd#important update
 
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
